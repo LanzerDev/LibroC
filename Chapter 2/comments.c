85c5e9a3c2f742d02c749e1d260663bfdd690de6
LanzerDev <78884322+LanzerDev@users.noreply.github.com>
2022-08-29 19:34:56 -0600

    actualizacion de la pc

---
+++ Chapter 2/comments.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void){
+
+    #if 0
+    printf("Hello world")
+    #endif /* 0 */
+    printf("comentarios");
+
+    // hola 
+    /*
+    * hola
+    */
+   
+
+    return 0;
+}
--- test.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(){
-    printf("hola mundo\n");
-    system("pause");
-    return 0;
-}
-
